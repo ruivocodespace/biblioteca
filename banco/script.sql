5364072cf21b78ea300a7304461efcb342e8ab4b
Peterson Ruivo <peterson_r_alves@estudante.sesisenai.org.br>
2026-04-09 22:09:38 -0300

    add username and telefone fields to usuarios table; update admin insertion

--- banco/script.sql
+++ banco/script.sql
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS livros (
     titulo    VARCHAR(150) NOT NULL,
     autor     VARCHAR(150) NOT NULL,
     genero    VARCHAR(255)     NULL,
-    ano       INT     		   NULL,
+    ano       INT              NULL,
     capa      VARCHAR(255)     NULL,
     criado_em TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id)
@@ -20,14 +20,18 @@ CREATE TABLE IF NOT EXISTS livros (
 CREATE TABLE IF NOT EXISTS usuarios (
     id        INT          NOT NULL AUTO_INCREMENT,
     nome      VARCHAR(150) NOT NULL,
+    username  VARCHAR(50)  NOT NULL,
     email     VARCHAR(150) NOT NULL,
+    telefone  VARCHAR(20)      NULL,
     senha     VARCHAR(255) NOT NULL,
-    tipo      ENUM('user','admin') NOT NULL DEFAULT 'admin',
+    tipo      ENUM('user','admin') NOT NULL DEFAULT 'user',
     criado_em TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id),
-    UNIQUE KEY uq_email (email)
+    UNIQUE KEY uq_email (email),
+    UNIQUE KEY uq_username (username)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Insere o usuário admin na tabela de usuarios
-INSERT INTO usuarios (nome, email, senha) VALUES 
-('Administrador', 'admin@email.com', 'admin123');
+-- Adicionado username 'admin', telefone e forçado o tipo 'admin'
+INSERT INTO usuarios (nome, username, email, telefone, senha, tipo) VALUES 
+('Administrador', 'admin', 'admin@email.com', '00000000000', 'admin123', 'admin');
