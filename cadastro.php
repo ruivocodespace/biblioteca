e8199ffc215a97fd9ea2a47bd212bf6661609500
Peterson Ruivo <peterson_r_alves@estudante.sesisenai.org.br>
2026-04-14 21:24:03 -0300

    formulario de cadastro corrigido, removido a confirmação de idade

--- cadastro.php
+++ cadastro.php
@@ -1,34 +1,38 @@
-<?
-?>
 <!DOCTYPE html>
 <html lang="pt-BR">
-  <head>
+
+<head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Home page Biblioteca</title>
     <link rel="stylesheet" href="assets/style.css" />
-  </head>  
-  <body>
+</head>
+
+<body>
     <div class="container_b">
-        <h2 class="cadastro">Cadastre-se</h2>    
-            <form action="/register" method="post">
-                <label class="nickname" for="new-username">Nickname:</label><br />
-                    <input class="input-form" type="text" id="new-username" name="new-username" required /><br />
-                <label class="pass" for="new-password">Senha:</label><br />
-                    <input class="input-form" type="password" id="new-password" name="new-password" required /><br />      
-                <label class="confirm-pass" for="confirm-password">Confirmar Senha:</label><br />
-                    <input class="input-form" type="password" id="confirm-password" name="confirm-password" required /><br />
-                <label class="name-1"for="name"> Nome Completo:</label><br />
-                    <input class="input-form" type="text" id="name" name="name" required /><br />
-                <label class="email-1" for="name">Email:</label><br />
-                    <input class="input-form" type="email" id="Email" formaction="/email" /><br/>
-                <label class="phone" for="name">Telefone:</label><br />
-                    <input class="input-form" type="numbet" id="Telefone" formaction="/phone" /><br>
-                <label class="age" type="checkbox" for="age">Maior de 18 anos?</label>
-                    <input type="checkbox" id="terms" name="terms" value="accepted" required /><br>
-                        <input class="btt-cadastro" type="submit" value="Registrar" />
-                    <button class="btt-cadastro" type="button" onclick="window.location.href='index.html'">Página Inicial</button>
-            </form>
-        </div>
-    </body>
+        <h2 class="cadastro">Cadastre-se</h2>
+        <form action="cadastro_usuario.php" method="post">
+            <label class="nickname" for="new-username">Nickname:</label><br />
+            <input class="input-form" type="text" id="username" name="username" required /><br />
+
+            <label class="name-1" for="name"> Nome:</label><br />
+            <input class="input-form" type="text" id="name" name="nome" required /><br />
+
+            <label class="email-1" for="name">Email:</label><br />
+            <input class="input-form" type="email" id="Email" name="email" /><br />
+
+            <label class="phone" for="name">Telefone:</label><br />
+            <input class="input-form" type="number" id="Telefone" name="telefone" /><br>
+
+            <label class="pass" for="new-password">Senha:</label><br />
+            <input class="input-form" type="password" id="new-password" name="senha" required /><br />
+
+            <label class="confirm-pass" for="confirma_senha">Confirmar Senha:</label><br />
+            <input class="input-form" type="password" id="confirm-password" name="confirma_senha" required /><br />
+
+            <input class="btt-cadastro" type="submit" value="Registrar" />
+        </form>
+    </div>
+</body>
+
 </html>
